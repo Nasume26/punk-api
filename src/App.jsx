67a2c5156a2c1ae8ed43677c6ece265772ef67d7
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-08-16 15:12:48 -0500

    Basic container and component creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import HomeContainer from "./containers/HomeContainer/HomeContainer";
 
+
+import { useState, useEffect } from "react"
 
 function App() {
   return (
     <div className="App">
+      <HomeContainer />
     </div>
   );
 }
---
+++ src/containers/HomeContainer/HomeContainer.jsx
@@ -0,0 +1,12 @@
+import "./HomeContainer.scss"
+
+const HomeContainer = () => {
+
+    return (
+        <>
+        <p>Home Works</p>
+        </>
+    )
+}
+
+export default HomeContainer
