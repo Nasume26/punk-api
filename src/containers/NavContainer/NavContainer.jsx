0b389a746b46b0ae3a2579af70935e0416d2b2c2
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-08-19 09:50:17 -0500

    Added comments for each functions use

--- src/containers/NavContainer/NavContainer.jsx
+++ src/containers/NavContainer/NavContainer.jsx
@@ -6,124 +6,135 @@ import { useEffect } from "react";
 import HomeContainer from "../HomeContainer/HomeContainer";
 
 const NavContainer = (props) => {
+
+    //Props and useState vars. There is a state for each checkbox. 
     const { beerData } = props;
-    const [search,setSearch] = useState("")
-    const [abvChecked, setAbvChecked] = useState(false)
-    const [classicChecked,setClassicChecked] = useState(false)
-    const [acidityChecked,setAcidityChecked] = useState(false)
-    const [mappedBeers, setMappedBeers] = useState ([])
+    const [search,setSearch] = useState("");
+    const [abvChecked, setAbvChecked] = useState(false);
+    const [classicChecked,setClassicChecked] = useState(false);
+    const [acidityChecked,setAcidityChecked] = useState(false);
+    const [mappedBeers, setMappedBeers] = useState ([]);
 
 
     //Handles input for the search box, cleans the input for filtering.
     const handleInput = (event) => {
         const cleanedInput = event.target.value.toLowerCase();
-        setSearch(cleanedInput)
-    }
+        setSearch(cleanedInput);
+    };
 
+    //handles the search boxes submit function. Disables it so dynamic search can function.
     const handleSubmit = (event) => {
         event.preventDefault();
-    }
+    };
 
     
-
+    //Variables that contain the final step of filtering, the number of results displayed, and the total 
+    //number of results to comb through
     const filteredBeer = mappedBeers.filter((beer) => {
         const lowerCaseBeer = beer.name.toLowerCase();
 
-        return lowerCaseBeer.includes(search)
-    })
-
-
+        return lowerCaseBeer.includes(search);
+    });
 
     const resultsCounter = filteredBeer.length;
 
     const totalCounter= beerData.length;
 
+
+
+    //This function populates the mapped beers array, and is used in conjunction with useEffect to 
+    //render that data on page load.
     const onPageLoadMap = () => {setMappedBeers(beerData.map((beer) => {
         return beer;
-    }))}
+    }))};
 
+    useEffect(onPageLoadMap, []);
     
-    useEffect(onPageLoadMap, [])
-    
-    
+ 
+    //This function allows the filters to populate the searchbox on check without having to enter
+    //a search term. Without it the filters would display nothing without search term data.
     const handleSearchBoxOnLoad = () => {
         if (search.length <= 0) {
-            setSearch("")
-        }
-    }
+            setSearch("");
+        };
+    };
 
     
-
+    //This function handles when a checkbox is ticked, and simultaneously runs the handleSearchBox func.
     const handleCheck = (event) => {
         if(event.target.name === "abv") {
             console.log(event.target.name)
             return setAbvChecked(!abvChecked),
-            handleSearchBoxOnLoad()
+            handleSearchBoxOnLoad();
            
         } else if (event.target.name === "classic range") {
             return setClassicChecked(!classicChecked),
-            handleSearchBoxOnLoad()
+            handleSearchBoxOnLoad();
         } else if (event.target.name === "acidity") {
             return setAcidityChecked(!acidityChecked),
-            handleSearchBoxOnLoad()
-        }
-
-       
-    }
+            handleSearchBoxOnLoad();
+        };
+    };
 
  
+    //The most complex function on the page, this function detects if any of the checkbox states have
+    //been set to true. If so it sets the Mapped Beers state to whatever filter is necessary.
+    //These can work individually or simultaneously. The useEffect runs this function every time
+    //a check state is updated.
     const handleCheckBooleans = () => {
          if(abvChecked === false && classicChecked === false && acidityChecked === false) {
             setMappedBeers(beerData.map((beer) => {
                 return beer;
-            }))
+            }));
         } else if (abvChecked === true  && classicChecked === true && acidityChecked === false) {
             const multiBeerFilterArray = beerData.filter ((beer) => {
-                return beer.abv >= 6
-            })
+                return beer.abv >= 6;
+            });
             setMappedBeers(multiBeerFilterArray.filter ((beer) => {
                 return beer.first_brewed.slice(-2) <= 10;
-            }))
+            }));
         } else if (abvChecked === true && acidityChecked === true && classicChecked === false) {
             const multiBeerFilterArray = beerData.filter ((beer) => {
-                return beer.abv >= 6
-            })
+                return beer.abv >= 6;
+            });
             setMappedBeers(multiBeerFilterArray.filter ((beer) => {
-                return beer.ph <= 4.0
-            }))
+                return beer.ph <= 4.0;
+            }));
         } else if (classicChecked === true && acidityChecked === true && abvChecked === false) {
             const multiBeerFilterArray = beerData.filter((beer) => {
-                return beer.first_brewed.slice(-2) <= 10
-            })
+                return beer.first_brewed.slice(-2) <= 10;
+            });
             setMappedBeers(multiBeerFilterArray.filter((beer) => {
-                return beer.ph <= 4.0
-            }))
+                return beer.ph <= 4.0;
+            }));
         } else if (classicChecked === true && acidityChecked === true && abvChecked === true) {
             const multiBeerFilterArray = beerData.filter((beer) => {
-                return beer.abv >= 6
-            })
+                return beer.abv >= 6;
+            });
             const multiBeerFilterArraySecondFilter = multiBeerFilterArray.filter((beer) => {
-                return beer.first_brewed .slice(-2) <= 10
-            })
+                return beer.first_brewed .slice(-2) <= 10;
+            });
             setMappedBeers(multiBeerFilterArraySecondFilter.filter((beer) => {
-                return beer.ph <= 4.0
-            }))
+                return beer.ph <= 4.0;
+            }));
         } else if (abvChecked === true && classicChecked ===false && acidityChecked === false) {
             setMappedBeers( beerData.filter((beer) => {
-                return beer.abv >= 6 
-            }))
+                return beer.abv >= 6;
+            }));
         } else if (classicChecked === true && abvChecked === false && acidityChecked === false ) {
             setMappedBeers ( beerData.filter((beer) => {
                 return beer.first_brewed.slice(-2) <= 10;
-            }))
+            }));
         } else if (acidityChecked === true && abvChecked === false && classicChecked === false) {
             setMappedBeers( beerData.filter((beer) => {
                 return beer.ph <= 4.0;
-            }))
-        }
-    }
+            }));
+        };
+    };
+
     useEffect(handleCheckBooleans, [abvChecked,classicChecked,acidityChecked])
 
+    
     return (
         <>
         <div className= "nav-container">
