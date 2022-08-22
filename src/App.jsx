204a98b3ba9bf4f957ee351d24be69f765676f02
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-08-22 17:30:01 -0500

    Started working on custom API button

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,13 @@ import { useState, useEffect } from "react"
 function App() {
 
   const [beerData, setBeerData] = useState();
+  const [customActive, setCustomActive] = useState(false)
 
 
   const getBeerData = () => {
     fetch ("https://api.punkapi.com/v2/beers?page=1&per_page=80")
     .then((res) => {
+      console.log(res)
       return res.json()
     })
     .then ((data) => {
@@ -21,9 +23,39 @@ function App() {
     })
   }
 
+  // const getCustomData = () => {
+  //   fetch ("192.168.0.198:3012/custom/")
+  //   .then((res) => {
+  //     return res.json()
+  //   })
+  //   .then ((data) => {
+  //     setBeerData(data)
+  //     console.log(data)
+  //   })
+  // }
+
+  const getCustomData = () => {
+    fetch ("localhost:3012/custom/",
+      {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+
+    }
+)
+    .then((res) => {
+      return res.json()
+    })
+    .then ((data) => {
+      setBeerData(data)
+      console.log(data)
+    })
+  }
   
 
   useEffect(getBeerData, [])
+ 
  
 
  
@@ -33,6 +65,7 @@ function App() {
 
   return (
     <div className="App">
+      <button onClick = {getCustomData}>CUSTOM</button>
       {beerData&& <NavContainer beerData={beerData} />}
       {beerData && <HomeContainer beerArr={beerData}/>}
     </div>
